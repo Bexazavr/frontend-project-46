1e546d8ba91914018c5930dddad42589a8e8bfbb
Vitaly <bexins@gmail.com>
2023-04-12 21:22:44 +0300

    feat: complete 2 of 8 steps, at 2nd project

---
+++ bin/gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+import genDiff from "../src/index.js";
+
+const program = new Command();
+
+program
+  .version("1.0.0")
+  .description("Compares two configuration files and shows a difference.")
+  .arguments("<filepath1> <filepath2>")
+  .option("-f, --format <type>", "output format")
+  .parse();
