5a64c15097607221719dd6a7c1e9ab9dc99a91fa
Vitaly <bexins@gmail.com>
2023-04-30 18:04:59 +0300

    refactor: del unnecessary

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import * as fs from 'node:fs';
 import * as path from 'path';
-import parse from './parsers.js';
+import getParsedData from './parsers.js';
 import formatData from './formatters/index.js';
 import buildDiffTree from './buildTree.js';
 
 const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
-const getFileExtenstion = (filepath) => path.extname(filepath).slice(1);
-const readFile = (filepath) => {
-  const data = fs.readFileSync(getAbsolutePath(filepath));
-  return parse(data, getFileExtenstion(filepath));
-};
+const getFileFormat = (filepath) => path.extname(filepath).slice(1);
+const getData = (filepath) => fs.readFileSync(getAbsolutePath(filepath));
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const data1 = readFile(filepath1);
-  const data2 = readFile(filepath2);
+  const data1 = getParsedData(getData(filepath1), getFileFormat(filepath1));
+  const data2 = getParsedData(getData(filepath2), getFileFormat(filepath2));
   const tree = buildDiffTree(data1, data2);
   return formatData(tree, formatName);
 };
--- src/parsers.js
+++ src/parsers.js
@@ -1,14 +1,14 @@
 import yaml from 'js-yaml';
 
-const parse = (file, extension) => {
-  switch (extension) {
+const parse = (file, format) => {
+  switch (format) {
     case 'yml':
     case 'yaml':
       return yaml.load(file);
     case 'json':
       return JSON.parse(file);
     default:
-      throw new Error('Wrong file extension!');
+      throw new Error('Wrong file format!');
   }
 };
 
